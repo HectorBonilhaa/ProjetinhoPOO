645fc96ff850e90cc28ae2bd9a522917c06e0539
sucodeuva288 <sucodeuva288@gmail.com>
2022-09-12 18:45:30 -0300

    atualizações no gerente

--- ProjetinhoPOO/src/funcionario/Funcionario.java
+++ ProjetinhoPOO/src/funcionario/Funcionario.java
@@ -1,6 +1,32 @@
 package funcionario;
 
-public class Funcionario 
+import cliente.Cliente;
+
+public abstract class Funcionario extends Cliente 
 {
+	protected String cargo;
+		
+	public Funcionario(String nome, String cpf, String senha) 
+	{
+		super(nome, cpf, senha);
+	}
+
+	public Funcionario(String nome, String cpf, String senha, String cargo) 
+	{
+		super(nome, cpf, senha);
+		this.cargo = cargo;
+	}
+
+	public String getCargo() 
+	{
+		return cargo;
+	}
+
+	public void setCargo(String cargo) 
+	{
+		this.cargo = cargo;
+	}
 	
+	
+		
 }
--- ProjetinhoPOO/src/funcionario/Gerente.java
+++ ProjetinhoPOO/src/funcionario/Gerente.java
@@ -2,5 +2,43 @@ package funcionario;
 
 public class Gerente extends Funcionario
 {
+	protected String cargo = "GERENTE";
+	
+	
+	public Gerente(String nome, String cpf, String senha) 
+	{
+		super(nome, cpf, senha);
+	}
 
+
+	public Gerente(String nome, String cpf, String senha, String cargo) 
+	{
+		super(nome, cpf, senha, cargo);
+	}
+
+
+	public String getCargo() 
+	{
+		return cargo;
+	}
+
+
+	public void setCargo(String cargo) 
+	{
+		this.cargo = cargo;
+	}
+
+
+	@Override
+	public String toString() 
+	{
+		return "Funcionario nome : " +getNome() +
+			   "\n          Cargo: " +  cargo + 
+			   "\n          CPF  : " + getCpf() + 
+			   "\n          senha: " + getSenha() + "\n\n";
+	}
+	
+	
+
+	
 }
