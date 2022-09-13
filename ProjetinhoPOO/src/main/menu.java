767613a9d693aae4a0ce5f36e76d47fa953d8e0d
sucodeuva288 <sucodeuva288@gmail.com>
2022-09-13 18:24:14 -0300

    parte do menu

--- ProjetinhoPOO/src/main/menu.java
+++ ProjetinhoPOO/src/main/menu.java
@@ -5,26 +5,27 @@ import java.util.List;
 import java.util.Scanner;
 
 import contas.Conta;
+import contas.ContaPoupanca;
 import funcionario.Funcionario;
 
 
 public class menu 
 {
 	static Scanner input = new Scanner(System.in);
-		
-	List<Conta> contas = new ArrayList<>();
 
 	
-	public static void main(String[] args)
-	{
-       
+	public static void main(String[] args) {
+		List<Conta> contasBanco = new ArrayList<>();
 		int x = 0;
 		Scanner leitor = new Scanner(System.in);
 		String cpf;
 		String senha;
 		
-		do 
-		{
+		Conta conta = new ContaPoupanca("dasda","123","1234",0, 5000.0,1);
+		contasBanco.add(conta);
+		
+		
+		do {
 			System.out.println("**********Bem Vindo a F.E.J.H.M**********");
 			System.out.println("-----------------------------------------");
 			System.out.println("-----------------------------------------");
@@ -35,47 +36,43 @@ public class menu
 			System.out.print("Senha: ");
 			senha = leitor.next();
 			
-			for(int i=0; i < contas.size() && x==0; i++)
-			{		
-					x = autenticar(i,cpf, senha);
-			        if(x == 1)
-			        	Conta contaUsuario = contas.get(i);
-
-			}
+			Conta contaUsuario = autenticar(cpf,senha, contasBanco);
 			
-			if(x == 0)
+			if(contaUsuario == null)
 			System.out.println("Senha ou cpf inválidos");
 		
-		}while(x == 0); 
-		
-		switch(contaUsuario.getTIPOU)
-		{
-		
-		case GERENTE.name: menuGerente();
-	       					   break;
-			case DIRETOR.name:  menuDiretor ();
-			    		       break;
-						  
-			case PRESIDENTE.name: menuPresidente();
-				                  break;
-				                  
-		    default: menuUsuario();
-		    	     break;
-		}
-		
+		}while(contaUsuario == null); 
+//		
+//		switch(contaUsuario.getTIPOU) {
+//		
+//		case GERENTE.name: menuGerente();
+//	       					   break;
+//			case DIRETOR.name:  menuDiretor();
+//			    		       break;
+//						  
+//			case PRESIDENTE.name: menuPresidente();
+//				                  break;
+//				                  
+//		    default: menuUsuario();
+//		    	     break;
+//		}
+//		
 		
 	}
 	
-	public int autenticar(int i, String cpf, String senha)
-	{
-			if(contas.get(i).getCpf.equalsIgnoreCase(cpf));
-				if(contas.get(i).getSenha.equalsIgnoreCase(senha))
-				{
-				    System.out.println("Bem Vindo "+ contas.get(i).getNome());
-				    return 1;
+	public static Conta autenticar(String cpf, String senha, List<Conta> contasUsuario) {
+		for (Conta conta : contasUsuario) {
+			if(conta.getCpf().equalsIgnoreCase(cpf));
+				if(conta.getSenha().equalsIgnoreCase(senha)) {
+					System.out.println("Bem Vindo "+ conta.getNome());
+					return conta;
+				}else {
+					return null;
 				}
-					else
-						return 0;
+		}
+		return null;
+		
+		
 	}
 
 }
